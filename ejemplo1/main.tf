ae631b1f2400425ea01f75c3a0e74f5d64dc434b
Ubuntu <ubuntu@ip-172-31-6-96.eu-west-1.compute.internal>
2021-02-02 18:52:09 +0000

    ejemplo docker completo

--- ejemplo1/main.tf
+++ ejemplo1/main.tf
@@ -15,14 +15,18 @@ provider "docker" { }
 resource "docker_container" "contenedor-ubuntu" {
   name  = "mi_contenedor_ubuntu"
   image = docker_image.imagen-ubuntu.latest
+  command = ["bash","-c","sleep 600"]
+  dynamic "volumes" {
+    for_each = var.volumenes
+    content {
+        volume_name    = contains( keys(volumes), "volume_name") ? volumes.value["volume_name"] : null
+#         volume_name    = keys(volumes), "volume_name") ? volumes.value["volume_name"] : "vacio"
+        host_path      = volumes.value["host_path"]
+        container_path = volumes.value["container_path"]
+    }
+  }
+  
 }
-/*
-2 volumenes dentro del contenedor:
-HOST
-/home/ubuntu/environment/cursoTerraform     >     /cursoTerraform
-/home/ubuntu/environment/ivan               >     /ivan
-*/
-
 
 resource "docker_image" "imagen-ubuntu" {
 //  OJO, tomo el último UBUNTU ¿?
@@ -30,3 +34,4 @@ resource "docker_image" "imagen-ubuntu" {
 #  name = var.nombre_imagen
   name = "${var.nombre_imagen}:${var.version_imagen}"
 }
+
