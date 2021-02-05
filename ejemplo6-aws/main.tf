c557e1aeb8484c9e3bc869666717e786180e56f0
Ubuntu <ubuntu@ip-172-31-6-96.eu-west-1.compute.internal>
2021-02-05 16:37:27 +0000

    ejemplo6

--- ejemplo6-aws/main.tf
+++ ejemplo6-aws/main.tf
@@ -18,9 +18,29 @@ resource "aws_instance" "mi-maquina" {
     instance_type = "t2.micro"
     key_name      = aws_key_pair.mis_claves.key_name # Se genera la relacion de dependencia
 #    key_name      = "claves_instancia_ivan"          # NO se genera la relacion de dependencia
+    
+    security_group = [
+        aws_security_group.reglas_red.name
+    ]
     tags          = {
         Name      = "Instancia_Ivan"
     }
+    
+    provisioner "remote_exec" {
+        inline = [
+                    "sudo apt update",
+                    "sudo apt install docker -y"
+                 ]
+        
+        connection {
+            host            = aws_instance.mi-maquina.public_ip
+            type            = "ssh"  # 22
+            user            = "ubuntu"
+            private_key     = tls_private_key.mi_clave_privada.private_key_pem 
+            timeout         = "1m"
+        }
+    }
+    
 }
 
 # Conectar ssh
@@ -34,5 +54,32 @@ resource "tls_private_key" "mi_clave_privada" {
 resource "aws_key_pair" "mis_claves" {
   key_name   = "claves_instancia_ivan"
   public_key = tls_private_key.mi_clave_privada.public_key_openssh
+}
+
+
+resource "aws_security_group" "reglas_red" {
+    name    = "Permitir acceso a la máquinas"
+    
+    ingress {
+        from_port = 22
+        to_port   = 22
+        protocol  = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
+
+
+output "clave_privada" {
+    value = tls_private_key.mi_clave_privada.private_key_pem 
+}
+output "clave_publica" {
+    value = tls_private_key.mi_clave_privada.public_key_pem 
 }
 
