2bb4e30a9808e8d25f2cbf90ae7056349bfce283
Ubuntu <ubuntu@ip-172-31-6-96.eu-west-1.compute.internal>
2021-02-05 15:35:21 +0000

    ejemplo5

---
+++ ejemplo5-aws-ec2/instancia/variables.tf
@@ -0,0 +1,16 @@
+
+variable "nombre_instancia" {
+    description = "Nombre de la instancia que voy a generar"
+    type        = string
+}
+
+variable "tipo_instancia" {
+    description = "Tipo de instancia que voy a generar"
+    type        = string
+    default     = "t2.micro"
+}
+
+variable "ami" {
+    description = "Nombre de la imagen que voy a instalar"
+    type        = string
+}
